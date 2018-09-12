88b9ed1bc6b720bb25696ee326cf83380bb0a180
KentoMoriwaki <kento@wantedly.com>
2018-09-13 00:39:34 +0900

    Use shell

--- gulpfile.js
+++ gulpfile.js
@@ -18,63 +18,9 @@ const runSequence = require("run-sequence");
 const getBuildConfig = require("./buildconfig");
 const babelrc = require("./.babelrc");
 
-// const config = getBuildConfig("ios");
-
 function handleError(err) {
   console.log(err);
 }
-
-// function normalizePath(path) {
-//   return path
-//     .replace(/^(?!\.(?:\/|\\))/, "./") // add ./ at beginning if not present
-//     .replace(/\\/g, "/"); // change path separators
-// }
-
-// function aliasify(aliases) {
-//   var reqPattern = new RegExp(/require\(['"]([^'"]+)['"]\)/g); // matches requires
-
-//   return eventStream
-//     .map((file, cb) => {
-//       if (!file.isNull()) {
-//         const content = file.contents.toString();
-//         if (reqPattern.test(content)) {
-//           file.contents = Buffer.from(
-//             content.replace(reqPattern, (req, oldPath) => {
-//               console.log(oldPath);
-//               if (!aliases[oldPath]) {
-//                 return req;
-//               }
-//               if (aliases[oldPath][0] === ".") {
-//                 const oldFolder = path.dirname(path.resolve(file.path));
-//                 const targetFile = path.resolve(aliases[oldPath]);
-//                 const newPath = path.resolve(oldFolder, targetFile);
-
-//                 return `require("${normalizePath(newPath)}")`;
-//               } else {
-//                 return `require("${normalizePath(aliases[oldPath])}")`;
-//               }
-//             })
-//           );
-//         }
-//       }
-//       cb(null, file);
-//     })
-//     .on("error", handleError);
-// }
-
-// gulp.task("babel", () =>
-//   gulp
-//     .src(src)
-//     .pipe(babel(babelrc))
-//     .on("error", handleError)
-//     // .pipe(aliasify(config.aliases))
-//     .pipe(gulp.dest(dist))
-//     .on("error", handleError)
-// );
-
-// gulp.task("build", callback => {
-//   runSequence(["babel"], callback);
-// });
 
 const platformConfigs = {
   web: {
@@ -160,7 +106,6 @@ gulp.task("copy", () => {
     .src(srcGlob)
     .pipe(platformify())
     .pipe(gulp.dest(paths.dist))
-    .pipe(tsProject({ typescript }))
     .on("error", handleError);
 });
 
@@ -171,11 +116,18 @@ gulp.task("watch-copy", () =>
 );
 
 gulp.task(
+  "ts-check",
+  shell.task(`node_modules/.bin/tsc -p tsconfig.${platform}.json`)
+);
+
+gulp.task(
   "build-webpack",
   shell.task(`node_modules/.bin/webpack --env.platform=${platform}`)
 );
 
-gulp.task("build", cb => runSequence(["copy", "build-webpack"], cb));
+gulp.task("build", cb =>
+  runSequence(["copy", "ts-check", "build-webpack"], cb)
+);
 
 gulp.task(
   "watch",
